de25701bde716347f05c7e67d87bc2a3375bb33c
nipunagg9873 <nipunagg9873@gmail.com>
2018-03-18 14:57:02 +0530

    setup heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const hbs=require('hbs');
 const fs=require('fs');
+const port=process.env.PORT ||3000;
 
 var app=express();
 app.set('view engine','hbs');
@@ -51,6 +52,6 @@ app.get('/bad',(req,res)=>{
     errorMessage:'kuchh gochi kia'
   });
 });
-app.listen(3000,()=>{
-  console.log('server is up');
+app.listen(port,()=>{
+  console.log(`server is up on port ${port}`);
 });
